9a3503d1bdf7a579ca596070b2baa16f0d96f284
Dalia Ramos and Kayla Ondracek <kayla@>
2018-01-09 16:54:43 -0800

    add ability to open and close all cards

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,17 @@ $(document).ready(function() {
   $(".definition").click(function() {
     $(this).hide();
   });
+
+  $("h2.open-cards").click(function() {
+    $(".definition").show();
+    $(this).hide();
+    $("h2.close-cards").show();
+  });
+
+  $("h2.close-cards").click(function() {
+    $(".definition").hide();
+    $(this).hide();
+    $("h2.open-cards").show();
+});
+
 });
